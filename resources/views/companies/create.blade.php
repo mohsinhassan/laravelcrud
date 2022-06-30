a43d0efbb639b284f0f21568442d5587e72bfdcc
mohsinhassan <mastermind_mohsin@hotmail.com>
2022-06-30 17:58:08 +0800

    check adding line

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -12,7 +12,7 @@
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left mb-2">
-                    <h2>Add Company</h2>
+                    <h2>Add Company github</h2>
                 </div>
                 <div class="pull-right">
                     <a class="btn btn-primary" href="{{ route('companies.index') }}"> Back</a>
